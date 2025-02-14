ae59f1d9821c96deea404ee40fee4c335fd5a1f4
edmooliveira29 <edmooliveira29@gmail.com>
2025-02-14 19:45:54 -0300

    config: update connection with server

--- index.ts
+++ index.ts
@@ -5,25 +5,19 @@ import { app } from "./src";
 import{env} from 'node:process';
 config()
 
-const PORT_SERVER = env.PORT_BACKEND_EXPRESS
-const PORT_DB = env.POSTGRES_PORT_DEVELOPMENT
-let HOST = env.POSTGRES_HOST_DEVELOPMENT
 
-if (env.NODE_ENV === "production") {
-  HOST = env.POSTGRES_HOST_PRODUCTION
-}
+const PORT_SERVER = env.PORT_BACKEND_EXPRESS
 
 async function startServer() {
   try {
-
     await conection.initialize();
-    console.log(`Database connection successfully established at ${HOST}:${PORT_DB}`);
+    console.log(`Database connection successfully!!`);
     app.listen(3000, () =>
       console.log(`Server is running successfully on port ${PORT_SERVER}!`)
     );
 
   } catch (error) {
-    console.error("❌ Erro ao conectar", error)
+    console.error("❌ Erro ao conectar: ", error)
   }
 }
 
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,38 +1,53 @@
 import "reflect-metadata"
 import path from "path";
-import { DataSource } from "typeorm"
+import { DataSource, DataSourceOptions } from "typeorm"
 import * as entities from "../models"
 import { config } from "dotenv";
 import { env } from 'node:process';
 config()
 
 let HOST = env.POSTGRES_HOST_DEVELOPMENT
-let PORT_DB = env.POSTGRES_PORT_DEVELOPMENT
+let PORT_DB = env.POSTGRES_PORT
 let USERNAME = env.POSTGRES_USERNAME_DEVELOPMENT
 let PASSWORD = env.POSTGRES_PASSWORD_DEVELOPMENT
 let DATABASE = env.POSTGRES_DATABASE_DEVELOPMENT
 
 let MIGRATIONS = [path.join(__dirname, "../database/migrations/*.{ts,js}")];
-
+let paramsToConect = {}
 if (env.NODE_ENV === "production") {
-    HOST = env.POSTGRES_HOST_PRODUCTION
-    PORT_DB = env.POSTGRES_PORT_PRODUCTION
-    USERNAME = env.POSTGRES_USERNAME_PRODUCTION
-    PASSWORD = env.POSTGRES_PASSWORD_PRODUCTION
-    DATABASE = env.POSTGRES_DATABASE_PRODUCTION
+    const databaseUrl = env.POSTGRES_DATABASE_URL_PRODUCTION;
+
+    if (!databaseUrl) {
+        throw new Error("DATABASE_URL não definida!");
+    }
+    paramsToConect = {
+        type: "postgres",
+        url: databaseUrl,
+        logging: false,
+        entities: Object.values(entities),
+        migrations: MIGRATIONS,
+        extra: {
+            ssl: {
+                rejectUnauthorized: false,
+            },
+            timezone: "America/Sao_Paulo",
+        },
+    }
+} else {
+    paramsToConect = {
+        type: "postgres",
+        host: HOST,
+        port: PORT_DB,
+        username: USERNAME,
+        password: PASSWORD,
+        database: DATABASE,
+        logging: false,
+        entities: Object.values(entities),
+        migrations: MIGRATIONS,
+        extra: {
+            timezone: "America/Sao_Paulo",
+        }
+    }
 }
 
-export const conection = new DataSource({
-    type: "postgres",
-    host: HOST,
-    port: Number(PORT_DB),
-    username: USERNAME,
-    password: PASSWORD,
-    database: DATABASE,
-    logging: false,
-    entities: Object.values(entities),
-    migrations: MIGRATIONS,
-    extra: {
-        timezone: "America/Sao_Paulo"
-    }
-})
+export const conection = new DataSource(paramsToConect as DataSourceOptions)
