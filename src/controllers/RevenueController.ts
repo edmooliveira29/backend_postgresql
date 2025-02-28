195bded3495ff8bdbf0a7a25b617424c69cd2e91
Edmo <edmooliveira29@gmail.com>
2025-02-28 17:11:14 -0300

    feat: crate controller about Revenue

---
+++ src/controllers/RevenueController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { QueryFailedError } from "typeorm";
+import { IRevenueService } from '../services/Revenue/Interface/IRepositoryService';
+
+export class RevenueController {
+  private revenueService: IRevenueService;
+
+  constructor(revenueService: IRevenueService) {
+    this.revenueService = revenueService
+  }
+
+  async create(request: Request, response: Response): Promise<void> {
+    try {
+      const revenue = await this.revenueService.create(request.body);
+      response.status(201).json(revenue);
+    }catch (err) {
+      response.status(500).json({ error: err.message });
+    }
+  }
+
+  async read(request: Request, response: Response): Promise<void> {
+    try {
+      const { id } = request.params;
+      const revenue = await this.revenueService.read(id);
+      response.json(revenue);
+    } catch (err) {
+      response.status(500).json({ error: err.message });
+    }
+  }
+
+  async update(request: Request, response: Response): Promise<void> {
+    try {
+      const { id } = request.params;
+      const revenue = await this.revenueService.update(request.body);
+      response.json(revenue);
+    } catch (err) {
+      response.status(500).json({ error: err.message });
+    }
+  }
+
+  async delete(request: Request, response: Response): Promise<void> {
+    try {
+      const { id } = request.params;
+      const revenue = await this.revenueService.delete(id);
+      response.json(revenue);
+    } catch (err) {
+      response.status(500).json({ error: err.message });
+    }
+  }
+
+  async readAll(request: Request, response: Response): Promise<void> {
+    try {
+      const revenues = await this.revenueService.readAll();
+      response.json(revenues);
+    } catch (err) {
+      response.status(500).json({ error: err.message });
+    }
+  }
+}
