4d1b2a5a56f39295213f9dc4b841edb415cbe06e
Edmo <edmooliveira29@gmail.com>
2025-03-02 11:39:02 -0300

    feat: create Service to CreditCard

---
+++ src/services/CreditCard/CreditCardService.ts
@@ -0,0 +1,35 @@
+import { ICreditCardService } from './Interface/IRepositoryCreditCard';
+import { CreditCards } from '../../models';
+import { ICreditCardRepository } from '../../repositories/CreditCard/interface/ICreditCardRepository';
+
+export class CreditCardService implements ICreditCardService {
+  private creditCardRepository: ICreditCardRepository
+
+  constructor(CreditCardRepository: ICreditCardRepository) {
+    this.creditCardRepository = CreditCardRepository
+  }
+
+  async create(CreditCard: CreditCards): Promise<CreditCards> {
+    const CreditCardRepository = await this.creditCardRepository.create(CreditCard)
+    return CreditCardRepository
+  }
+
+  async read(id: string): Promise<CreditCards> {
+    const CreditCard = await this.creditCardRepository.read(id)
+    return CreditCard
+  }
+
+  async update(CreditCard: CreditCards): Promise<CreditCards | null> {
+    const CreditCardRepository = await this.creditCardRepository.update(CreditCard)
+    return CreditCardRepository
+  }
+
+  async delete(id: string): Promise<{ deleted: number } | null> {
+    return await this.creditCardRepository.delete(id)
+  }
+
+  async readAll(): Promise<CreditCards[]> {
+    const CreditCards = await this.creditCardRepository.readAll()
+    return CreditCards
+  }
+}
